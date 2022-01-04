41b8e2e3cf7b88697c85c91afb7c16b48b6742fe
Lutfi Haridha <lutfiharidha1@gmail.com>
2022-01-04 17:34:47 +0700

    Add Title and icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={"bg-gray-100"}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <link rel="icon" href="lutfi.ico" type="image/x-icon" />
+        <title>Lutfi Haridha</title>
+      </Head>
+      <div className={"bg-gray-100"}>
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
 
